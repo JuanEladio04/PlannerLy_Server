fae93b7983588ee10be5d45312799caf9e7ba9b1
JuanEladio04 <113886575+JuanEladio04@users.noreply.github.com>
2024-07-24 17:55:31 +0200

    feat(DataBase): The database sql has been added

---
+++ DataBase.sql
@@ -0,0 +1,209 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: PlannerLy
+-- ------------------------------------------------------
+-- Server version	8.3.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `belongs`
+--
+
+DROP TABLE IF EXISTS `belongs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `belongs` (
+  `uid` int NOT NULL,
+  `wsid` int NOT NULL,
+  PRIMARY KEY (`uid`,`wsid`),
+  KEY `fk_work_space_idx` (`wsid`),
+  CONSTRAINT `fk_user_id` FOREIGN KEY (`uid`) REFERENCES `usuarios` (`id`),
+  CONSTRAINT `fk_workSpace_id` FOREIGN KEY (`wsid`) REFERENCES `work_space` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `belongs`
+--
+
+LOCK TABLES `belongs` WRITE;
+/*!40000 ALTER TABLE `belongs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `belongs` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `nota`
+--
+
+DROP TABLE IF EXISTS `nota`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `nota` (
+  `pid` int NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `subtitle` varchar(300) DEFAULT NULL,
+  `content` text,
+  PRIMARY KEY (`pid`),
+  CONSTRAINT `fk_P_N_ID` FOREIGN KEY (`pid`) REFERENCES `publication` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `nota`
+--
+
+LOCK TABLES `nota` WRITE;
+/*!40000 ALTER TABLE `nota` DISABLE KEYS */;
+/*!40000 ALTER TABLE `nota` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `publication`
+--
+
+DROP TABLE IF EXISTS `publication`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `publication` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `wsid` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_workSpace_Id_idx` (`wsid`),
+  CONSTRAINT `fk_p_workSpace_Id` FOREIGN KEY (`wsid`) REFERENCES `work_space` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `publication`
+--
+
+LOCK TABLES `publication` WRITE;
+/*!40000 ALTER TABLE `publication` DISABLE KEYS */;
+/*!40000 ALTER TABLE `publication` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `recordatorio`
+--
+
+DROP TABLE IF EXISTS `recordatorio`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `recordatorio` (
+  `pid` int NOT NULL,
+  `date` date NOT NULL,
+  `hour` time NOT NULL,
+  `completed` tinyint NOT NULL,
+  PRIMARY KEY (`pid`),
+  CONSTRAINT `fk_P_R_ID` FOREIGN KEY (`pid`) REFERENCES `publication` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `recordatorio`
+--
+
+LOCK TABLES `recordatorio` WRITE;
+/*!40000 ALTER TABLE `recordatorio` DISABLE KEYS */;
+/*!40000 ALTER TABLE `recordatorio` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tarea`
+--
+
+DROP TABLE IF EXISTS `tarea`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tarea` (
+  `pid` int NOT NULL,
+  `state` set('in_process','pending','completed') NOT NULL,
+  `name` varchar(200) DEFAULT NULL,
+  `description` text,
+  PRIMARY KEY (`pid`),
+  CONSTRAINT `fk_P_T_ID` FOREIGN KEY (`pid`) REFERENCES `publication` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tarea`
+--
+
+LOCK TABLES `tarea` WRITE;
+/*!40000 ALTER TABLE `tarea` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tarea` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuarios` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(45) NOT NULL,
+  `name` varchar(200) DEFAULT NULL,
+  `second_name` varchar(200) DEFAULT NULL,
+  `f_nac` date DEFAULT NULL,
+  `phone_number` int DEFAULT NULL,
+  `email` varchar(300) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `work_space`
+--
+
+DROP TABLE IF EXISTS `work_space`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `work_space` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(300) NOT NULL,
+  `description` text,
+  `work_spacecol` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `work_space`
+--
+
+LOCK TABLES `work_space` WRITE;
+/*!40000 ALTER TABLE `work_space` DISABLE KEYS */;
+/*!40000 ALTER TABLE `work_space` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-02-02 13:40:44
